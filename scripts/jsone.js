610808a996d54bbc90dfa39cd00085637c37ffe5
Thomas Devol <vajrapani666@ECHO3.local>
2011-06-11 22:54:38 -0500

    Added documentation

--- scripts/jsone.js
+++ scripts/jsone.js
@@ -1,7 +1,41 @@
-
+/*
+ * JSON Editor 
+ * Author: Thomas W Devol (vajrapani666)
+ * 
+ * Purpose:
+ * 	Provide a way to edit data of any type or complexity (within browser limits)
+ * Usage: 
+ * 	var jse=new JSONEditor();
+ * 	jse.load({foo:'bar'});
+ * 	jse.render($("#target"));
+ * About @tunnel,@context: 
+ * 	The whole editor essentially recursively parses its _object creating a DOM tree on the element passed to render
+ * 	To provide editing capabilities, As the tree is created, certain points have bound events that take a "tunnel"
+ * 	argument
+ * 	The tunnel is a function that calls its parent, this allows the recursion path to any node to be "traversed backwards"
+ * 	up to the root. 
+ * 	This reverse-recursive pattern generates a path to the node that originated the sequence of 
+ * 	calls to tunnel (the leaf data-manipulation call) 
+ * 	The tunnel takes two arguments, a string and a function.
+ * 	The string builds the path reverse-recursively terminating in _render
+ * 	The function passed to tunnel is created at the leaf-level and passed all the way to the root level
+ * 	allowing it to take advantage of scope-changes through the recursive function path
+ *
+ * 	The function is usually called dfunc and it is created at the leaf, passed up the tree and called from the root
+ * 	with the whole object as the first argument, the generated path as the second, and the node value as the last
+ *	The dfunc generates a javascript string which will modify the _object 
+ *
+ *	So context/tunnel is essentially an execution path back to the root along which can be passed a callback with curried
+ *	arguments from the leaf to be called from the root
+ * 	
+ *
+ */
 var JSONEditor = function() {
 	var _object={};
 	var _state={};
+	//returns the type of @obj as a string
+	//returns unknown if type cannot be determined
+	//relies heavily on underscore.js 
 	var _type=function( obj) {
 		if (_.isArray(obj)) return "array";
 		if (_.isBoolean(obj)) return "bool";
@@ -12,12 +46,21 @@ var JSONEditor = function() {
 		if (Object.prototype.toString.call(obj)=="[object Object]") return "hash";
 		return "unknown";
 	}
+	// render an editor for the edit-value dialog
+	// @param target (jQuery Object) - Where to place the editor
+	// @param val			 - The value the editor will take initially
+	// @param type (string)		- The initial type
+	// @param on_submit (function)  - A callback to which is passed the new value once a new value is chosen
 	var _render_editor=function(target,val,type,on_submit){
-		target.empty();
+		//always start with blank slate
+		target.empty(); 
+		//create a raw value control
 		var label=$("<label>",{text:"Raw Value"});
 		var text_editor=$("<input>",{val: val,id:"value-target"});	
 		target.append(label);
 		target.append(text_editor);
+
+		//create controls / behaviors dependent on the type selected
 		switch (type) {
 			case 'hash':
 				on_submit("{}");
@@ -80,8 +123,14 @@ var JSONEditor = function() {
 
 
 	}
+	// Creates a dialog allowing the user to edit a value
+	// @param val - The value being edited
+	// @param type (string) - The current type of the value
+	// @param on_submit (function) - A callback taking the new value 
 	var _create_editor=function(val,type,on_submit) {
 		$('#leaf-editor').remove();
+
+		//setup html structure
 		var editor=$("<div/>",{id:"leaf-editor"})
 		var form=$("<form/>");
 		var fieldset=$("<fieldset/>");
@@ -90,6 +139,8 @@ var JSONEditor = function() {
 		var container=fieldset;
 		container.append($("<label>",{text:"Data Type"}));
 		var select_menu=$("<select>");
+
+		//setup the data types menu
 		options={
 			'string':"Text",
 			'number':"Number",
@@ -104,12 +155,13 @@ var JSONEditor = function() {
 			select_menu.append(option);
 		});
 		var edit_area=$("<div>",{id:'leaf-editor-value'});
-	
+		//when the data type changes, render an appropriate editor
 		select_menu.bind('change',function() {
 			_render_editor(edit_area, val, $(this).val(),on_submit);
 		});
 		container.append(select_menu);
 		container.append(edit_area);
+		//setup the dialog
 		editor.dialog({
 			title:"Edit Value",
 			height: 300,
@@ -134,16 +186,21 @@ var JSONEditor = function() {
 		editor.dialog('open');
 		select_menu.trigger('change');
 	}
+	// A refactored function which executes when a parent node is hovered over
+	// Shows a menu displaying an add and delete option
+	// @param label (jQuery Object) - The label to attach the menu to
+	// @param child_type (string) - We need this to pass to _add_node so that we know if we need to ask for a key name
+	// @tunnel - See tunnel
 	var _parent_hover=function(label,child_type,tunnel) {
 			return function() {
 						var node_menu=$("<span>",{class:'ui-widget-header ui-corner-all node-menu'});
 						var add_button=$("<button>",{title:"Add Node"}).button({
 							text: false,
-							icons:{primary: "ui-icon-seek-start"}
+							icons:{primary: "ui-icon-circle-plus"}
 						}).click(_add_node(child_type,tunnel));
 						var del_button=$("<button>",{title:"Delete Node"}).button({
 							text:false,
-							icons:{primary: "ui-icon-stop"}
+							icons:{primary: "ui-icon-circle-close"}
 						}).click(_del_node(tunnel));
 						node_menu.append(add_button);
 						node_menu.append(del_button);
@@ -151,6 +208,9 @@ var JSONEditor = function() {
 						label.append(node_menu);
 					}
 	};
+
+	//generates a generic delete node function
+	//@param context (function) the context to call and pass back the dfunc up the tree
 	var _del_node=function(context) {
 		return function() {
 			var dfunc=function(obj,path,node) {
@@ -162,6 +222,8 @@ var JSONEditor = function() {
 		}
 
 	};
+
+	//adds a node defaulting to N/A/null
 	var _add_node=function(type,context) {
 		console.log("Creating function for "+type);
 		return function() {
@@ -178,6 +240,13 @@ var JSONEditor = function() {
 			context("",dfunc);
 		}
 	};
+
+
+	//Wraps the leaf-values of the tree , creates the editor on hover
+	//@param context (function) - See top
+	//@param obj - The actual value 
+	//@param view (jQuery Object) - The visual representation of the object
+	//@param type (string) - The type of the object required to pass to _create-editor 
 	var _value_wrapper=function(context,obj,view,type) {
 		var env=this;
 		var edit=function() {
@@ -202,13 +271,13 @@ var JSONEditor = function() {
 			var edit_button=$("<button>",{title:"Edit"}).button({
 				text:"Edit",
 				icons: {
-					primary:"ui-icon-seek-next"
+					primary:"ui-icon-circle-triangle-e"
 				}
 			}).click(edit);
 			var del_button=$("<button>",{title:"Remove"}).button({
 				text:"Remove",
 				icons: {
-					primary: "ui-icon-seek-prev"
+					primary: "ui-icon-circle-close"
 				}
 			}).click(_del_node(context));
 			leaf_menu.append(edit_button);
@@ -219,23 +288,30 @@ var JSONEditor = function() {
 		});
 		return view;	
 	}
-
+	
+	//recursive core function to recurively display @obj 
+	//recursion begins in public method render
 	var _display=function(context,obj,type) {
 		var el;
+		//initialize the path back to the caller's context function
 		var tunnel=function(str,dfunc) {
 			context(str,dfunc);
 		};
+
 		if (type=='hash') {	
 			el=$("<dl/>");
 			_.each(obj,function(val,key,o) {
 				var child_type=_type(val);
 				var label=$("<dt>",{text:key});
 				var is_parent=child_type=='hash' || child_type=='array';
+				//modify the tunnel to build the path for encounteered hashes
 				var tunnel=function(str,dfunc) {
 					context("['"+key+"']"+str.toString(),dfunc);
 				}
+
 				var value_el=$("<dd>",{'data-type': child_type}).append(_display(tunnel,val,child_type));
 				if (is_parent) {
+					//toggle on click
 					label.click(function() {
 						value_el.toggle();
 					});
@@ -251,14 +327,19 @@ var JSONEditor = function() {
 		} else if (type=='array') {
 			el=$("<ul/>");
 			_.each(obj,function(val,i,o) {
+
 				var child_type=_type(val);
 				var is_parent=child_type=='hash' || child_type=='array';
 				var label=$("<div/>",{text:"-",title:i});
+
+				//modify the tunnel to create path entry for array element i
 				tunnel=function(str,dfunc) {
 					context("["+i+"]"+str.toString(),dfunc);
 				}
 				var value_el=_display(tunnel,val,child_type);
 				var list_el=$("<li>",{'data-type':child_type});
+
+				//assign special behavior if this element is a parent
 				if (is_parent) {
 					label.click(function() {
 						value_el.toggle();
@@ -285,19 +366,31 @@ var JSONEditor = function() {
 		return el;
 
 	}
+
+	//public interface for JSEditor
 	return {
+		//takes a javascript hash, this loads the main object to be displayed into the editor
 		load: function(obj) {
 			_object={"/":obj};
 		},
+		//returns the current value of the editor
 		value: function() {
 			return _object["/"];
 	       },
+		//render the _object to the jQuery Object @target
 		render: function(target) {
-			var instance=this;
-			target.empty().append(_display(function(str,dfunc) {
+			var instance=this; //need to pass through the closure 
 
+			//the first argument to display here is actually the root terminator for calls
+			//initiated at leaf-nodes via context("",dfunc). 
+			target.empty().append(_display(function(str,dfunc) {
+					//use the path stored in str to determine the path to the node that 
+					//initiated the call up the tree .. determine its value
 					var node=eval("_object"+str);
+					//call the passed up callback function, it is called dfunc for delta-function, change-function
+					//the function should change _object somehow.
 					dfunc(_object,str,node);
+					//re-render the object
 					instance.render(target);
 
 			},_object,_type(_object)));
